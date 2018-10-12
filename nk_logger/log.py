2de87d9c1d8e312fea3aef911cb2bf9f15eb7d92
Craig Corcoran <craig@newknowledge.io>
2018-10-12 13:12:57 -0500

    add logger code as pip-installable package

---
+++ nk_logger/log.py
@@ -0,0 +1,34 @@
+""" Sets up logging config for stream handlers sending info-level logs and
+below to stdout and warning-level and above to stderr, provides get_logger
+function. Designed to work well with datadog. """
+
+import logging
+import sys
+
+from pythonjsonlogger import jsonlogger
+
+
+def get_logger(name, level="INFO"):
+    formatter = jsonlogger.JsonFormatter(timestamp=True, reserved_attrs=[])
+    out_handler = logging.StreamHandler(sys.stdout)
+    out_handler.setLevel(level)
+    out_handler.setFormatter(formatter)
+    # filter out warning and above, send to stderr instead
+    out_handler.addFilter(lambda record: record.levelno < logging.WARNING)
+
+    err_handler = logging.StreamHandler(sys.stderr)
+    err_handler.setLevel(logging.WARNING)
+    err_handler.setFormatter(formatter)
+
+    # set root log level to see other packages info/debug logs (e.g. adl, kafka)
+    # root = logging.getLogger()
+    # root.setLevel(LOG_LEVEL)
+
+    logger = logging.Logger(name, level=level)
+    logger.addHandler(out_handler)
+    logger.addHandler(err_handler)
+    return logger
+
+
+_logger = get_logger(__name__)
+_logger.info("initialized logger")
